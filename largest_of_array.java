997dcf499c9f4c84374ac13d40d3471205be19a4
Advith-B <advithadvi31@gmail.com>
2026-05-05 10:00:49 +0530

    Create largest_of_array.java

---
+++ largest_of_array.java
@@ -0,0 +1,13 @@
+public class Main
+{
+	public static void main(String[] args) {
+        int[] arr = {70, 32, 45, 90, 98};
+        int max = arr[0];
+        for (int i = 1; i < arr.length; i++) {
+            if (arr[i] > max) {
+                max = arr[i];
+            }
+        }
+        System.out.println("Largest element is: " + max);
+    }
+}
