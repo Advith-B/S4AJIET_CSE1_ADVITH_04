fb9ad3047f938dd475e123584a43ab0007fb318a
Advith-B <advithadvi31@gmail.com>
2026-05-06 09:25:59 +0530

    Create string_reverse.java

---
+++ string_reverse.java
@@ -0,0 +1,10 @@
+public class Main
+{
+	public static void main(String[] args) {
+	String s="bob";
+	String rev=" ";
+	for(int i=0;i<s.length();i++){
+	    rev=s.charAt(i)+rev;
+	}
+	System.out.println("Reverse is : "+rev);	}
+}
